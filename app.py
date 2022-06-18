7c016d6025c02ade42a8d64c3cf96da0dd11d188
Ben <24gluck_benjamin@dublinstudents.net>
2022-06-18 14:23:40 -0400

    added publishing events

--- app.py
+++ app.py
@@ -2,18 +2,23 @@
 import firebase_admin
 import pyrebase
 import json
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, db
 from flask import Flask, request
 from functools import wraps
+from dotenv import load_dotenv
+import os
 
 #App configuration
+
 app = Flask(__name__)
 #Connect to firebase
+load_dotenv()
 cred = credentials.Certificate('fbadminconfig.json')
-firebase = firebase_admin.initialize_app(cred)
+firebase = firebase_admin.initialize_app(cred, 
+{
+    'databaseURL': os.environ['DATABASE']
+})
 pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
-#Data source
-users = [{'uid': 1, 'name': 'Noah Schairer'}]
 
 def check_token(f):
     @wraps(f)
@@ -27,12 +32,6 @@ def check_token(f):
             return {'message':'Invalid token provided.'},400
         return f(*args, **kwargs)
     return wrap
-
-#Api route to get users
-@app.route('/api/userinfo')
-@check_token
-def userinfo():
-    return {'data': users}, 200
 
 #Api route to sign up a new user
 @app.route('/api/signup')
@@ -63,5 +62,33 @@ def token():
         return {'token': jwt}, 200
     except:
         return {'message': 'There was an error logging in'},400
+
+#connect to firebase database and publish json data
+@app.route('/api/publish', methods=['POST', 'GET'])
+@check_token
+def publish_data():
+    if request.method == 'GET':
+        return {'message': 'Data published'},200
+    ref = db.reference("/events")
+    user = auth.verify_id_token(request.headers['authorization'])
+    data = {
+        'userid': user['uid'],
+        'name': request.form.get('name'),
+        'location': request.form.get('location'),
+        'description': request.form.get('description'),
+    }
+    ref.push().set(data)
+    return {'message': 'Data published'},200
+
+@app.route('/api/getdata')
+def get_data():
+    ref = db.reference("/events")
+    data = ref.get()
+    return {'data': data}, 200
+
+def get_user_info(uid):
+    return users[uid]
+
+
 if __name__ == '__main__':
     app.run(debug=True)
